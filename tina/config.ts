9d18682073d3794ac97e0ef7c2314cbf43e720f4
sondq <sondq@gmail.com>
2024-12-05 20:37:53 +0700

    add  tina id

--- tina/config.ts
+++ tina/config.ts
@@ -6,7 +6,7 @@ const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || 'main'
 
 export default defineConfig({
 	branch,
-	clientId: null, // Get this from tina.io
+	clientId: 'fa7ca7f0-03f6-49ac-9bf5-0f156c1c5dd0', // Get this from tina.io
 	token: null, // Get this from tina.io
 
 	build: {
